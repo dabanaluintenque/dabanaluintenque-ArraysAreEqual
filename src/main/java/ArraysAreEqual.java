ae061f26edfc2cbd120d45369029ca4274b2c262
Dabana Intenque <Dabanaluintenque55@gmail.com>
2024-03-15 23:20:30 +0000

    Week 4 coding chalenge Arrays Are Equal
    Done!

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -1,4 +1,7 @@
 //see test cases.
+
+import java.util.Arrays;
+
 public class ArraysAreEqual {
     /**
      * Check if two arrays are equal to each other. Return true if all of the values in a are
@@ -15,6 +18,15 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        return false;
+
+        boolean checkEquality = false;
+
+        if (Arrays.equals(a,b)) {
+
+            checkEquality = !checkEquality;
+        }
+
+        return checkEquality;
+        
     }
 }
